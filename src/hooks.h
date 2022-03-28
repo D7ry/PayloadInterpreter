4a912698830d6f04adf791f387bdc9e3598af977
d7ry <92609548+D7ry@users.noreply.github.com>
2022-03-27 18:55:38 -0700

    playerUpdate hook

--- src/hooks.h
+++ src/hooks.h
@@ -4,12 +4,18 @@
 class Hook_MainUpdate
 {
 public:
+
 	static void install() {
 		auto& trampoline = SKSE::GetTrampoline();
-
+#if ANNIVERSARY_EDITION
+		REL::Relocation<uintptr_t> hook{ REL::ID(36544) };  // 5D29F0, main loop
+		_Update = trampoline.write_call<5>(hook.address() + 0x160, Update);
+#else
 		REL::Relocation<uintptr_t> hook{ REL::ID(35551) };  // 5AF3D0, main loop
-
 		_Update = trampoline.write_call<5>(hook.address() + 0x11F, Update);
+#endif
+
+		
 		INFO("Main Update hook installed.");
 	}
 
@@ -23,11 +29,31 @@ private:
 	static inline REL::Relocation<decltype(Update)> _Update;
 
 };
+
+class Hook_PlayerUpdate
+{
+public:
+	static void install() {
+		REL::Relocation<std::uintptr_t> PlayerCharacterVtbl{ RE::Offset::PlayerCharacter::Vtbl };
+		_Update = PlayerCharacterVtbl.write_vfunc(0xAD, Update);
+		INFO("Player update hook installed");
+	}
+private:
+	
+	static void Update(RE::PlayerCharacter* a_this, float a_delta) {
+		if (payloadManager::hasAsyncTask) {
+			payloadManager::update();
+		}
+		_Update(a_this, a_delta);
+	}
+	static inline REL::Relocation<decltype(Update)> _Update;
+};
 class Hooks {
 public:
 
 	static void install() {
-		SKSE::AllocTrampoline(1 << 4);
-		Hook_MainUpdate::install();
+		SKSE::AllocTrampoline(1 << 5);
+		//Hook_MainUpdate::install();
+		Hook_PlayerUpdate::install();
 	}
 };
--- src/offsets.h
+++ src/offsets.h
@@ -4,11 +4,19 @@ class offsets
 public:
 
 	typedef void(_fastcall* _shakeCamera)(float strength, RE::NiPoint3 source, float duration);
-	static inline REL::Relocation<_shakeCamera> shakeCamera{ REL::ID(32275) };
-
 	typedef void(_fastcall* _setIsGhost)(RE::Actor* actor, bool isGhost);
-	static inline REL::Relocation<_setIsGhost> setIsGhost{ REL::ID(36287) };
 
+#if ANNIVERSARY_EDITION
+	
+#else 
+	static inline REL::Relocation<_shakeCamera> shakeCamera{ REL::ID(32275) };
+	static inline REL::Relocation<_setIsGhost> setIsGhost{ REL::ID(36287) };
 	static inline float* g_deltaTime = (float*)REL::ID(523660).address();                            // 2F6B948
 	static inline float* g_deltaTimeRealTime = (float*)REL::ID(523661).address();                  // 2F6B94C
+#endif
+	
+
+	
+
+
 };
